b501b3902fbda549d66fd9ff5198376aff27de64
Habtamu <habtamuwolde92@gmail.com>
2023-05-13 11:53:02 +0530

    added data for dymaic load in the future

---
+++ index.js
@@ -0,0 +1,61 @@
+const mobileData = [
+    {
+        txt: 'Deep earth',
+        img: 'blah blah'
+    },
+    {
+        txt: 'Night arcade',
+        img: ''
+    },
+    {
+        txt: 'Soccer team VR',
+        img: ''
+    },
+    {
+        txt: 'From up above VR',
+        img: ''
+    },
+    {
+        txt: 'Pocket borealis',
+        img: ''
+    },
+    {
+        txt: 'The curiosity',
+        img: ''
+    },
+    {
+        txt: 'Make it fisheye',
+        img: ''
+    },   
+]
+const desktopData = [
+    {
+        txt: 'Deep earth',
+        img: 'blah blah'
+    },
+    {
+        txt: 'Night arcade',
+        img: ''
+    },
+    {
+        txt: 'Soccer team VR',
+        img: ''
+    },
+    {
+        txt: 'From up above VR',
+        img: ''
+    },
+    {
+        txt: 'Pocket borealis',
+        img: ''
+    },
+    {
+        txt: 'The curiosity',
+        img: ''
+    },
+    {
+        txt: 'Make it fisheye',
+        img: ''
+    },   
+]
+console.log( mobileData)
